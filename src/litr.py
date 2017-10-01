2ddb28f7e8f8a173e61869e5dcf5d396ae9a1b10
Vova Abdrakhmanov <369565@gmail.com>
2017-10-01 22:04:05 +0300

    rewrite higher neighbour function

--- src/litr.py
+++ src/litr.py
@@ -7,60 +7,52 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 def greatest_neighbour(x, y, arr):
-	neighbours = [[x, y, arr[x, y]]]
-	if y > 0:
-			neighbours.append([x, y-1, arr[x, y-1]])
-	if y < arr.shape[1] - 1:
-		neighbours.append([x, y+1, arr[x, y+1]])
-	if x > 0:
-		neighbours.append([x-1, y, arr[x-1, y]])
-		if y > 0:
-			neighbours.append([x-1, y-1, arr[x-1, y-1]])
-		if y < arr.shape[1] - 1:
-			neighbours.append([x-1, y+1, arr[x-1, y+1]])
-	if x < arr.shape[0] - 1:
-		neighbours.append([x+1, y, arr[x+1, y]])
-		if y > 0:
-			neighbours.append([x+1, y-1, arr[x+1, y-1]])
-		if y < arr.shape[1] - 1:
-			neighbours.append([x+1, y+1, arr[x+1, y+1]])
-	m = max(neighbours, key = lambda x: x[2])
-	if m[0] != x or m[1] != y:
-		return m[0], m[1]
+	# сдвиги по осям
+	xshifts = np.arange(-1, 2)
+	yshifts = np.arange(-1, 2)
+	# индексы
+	xs = x + xshifts
+	ys = y + yshifts
+	ys = ys[ys < arr.shape[0]]
+	xs = xs[xs < arr.shape[1]]
+	ys = ys[ys >= 0]
+	xs = xs[xs >= 0]
+	xs, ys = np.meshgrid(xs, ys)
+	# положение максимума
+	i = np.unravel_index(np.argmax(arr[ys, xs]), xs.shape)
+	if xs[i] != x or ys[i] != y:
+	 	return xs[i], ys[i]
 	return None
 
-im = imread("../img/test3.jpg")
-lightness = (im.min(axis=2) + im.max(axis=2)) / 2
-del im
+if __name__ == '__main__':
+	# получаем яркость картинки
+	lightness = imread("../img/test1.jpg")
+	lightness = (lightness.min(axis=2) + lightness.max(axis=2)) / 2
 
-lightness = gaussian_filter(lightness, 3)
-u, v = np.gradient(lightness)
+	# сглаживаем
+	lightness = gaussian_filter(lightness, 3)
 
-x = np.arange(lightness.shape[0])
-y = np.arange(lightness.shape[1])
-x, y = np.meshgrid(y, x)
-gr = np.sqrt(u * u + v * v)
-gr /= gr.max()
+	x = np.arange(lightness.shape[0])
+	y = np.arange(lightness.shape[1])
+	x, y = np.meshgrid(x, y)
+	
+	plt.contourf(x, y, lightness)
 
-plt.contourf(x, y, lightness)
+	results = set()
 
-results = set()
+	for i in range(1000):
+		cx = np.random.choice(np.arange(lightness.shape[0]))
+		cy = np.random.choice(np.arange(lightness.shape[1]))
 
-for i in range(1000):
-	cx = np.random.choice(np.arange(lightness.shape[0]))
-	cy = np.random.choice(np.arange(lightness.shape[1]))
-
-	r = greatest_neighbour(cx, cy, lightness)
-	while r:
-		cx, cy = r
 		r = greatest_neighbour(cx, cy, lightness)
-	results.add((cx, cy, lightness[cx, cy]))
+		while r:
+			cx, cy = r
+			r = greatest_neighbour(cx, cy, lightness)
+		results.add((cx, cy, lightness[cy, cx]))
 
-results = list(results)
-results.sort(key=lambda x: -x[2])
-s1, s2 = results[:2]
-plt.plot([s1[1], s2[1]], [s1[0], s2[0]], color="k")
-plt.title("Distance: %d px" % ((s1[0] - s2[0])**2 + (s1[1] - s2[1])**2) ** .5)
-# plt.streamplot(x, y, v, u, linewidth=lw, color='k')
-plt.show()
-
+	results = list(results)
+	results.sort(key=lambda x: -x[2])
+	s1, s2 = results[:2]
+	plt.plot([s1[0], s2[0]], [s1[1], s2[1]], color="k")
+	plt.title("Distance: %d px" % ((s1[0] - s2[0])**2 + (s1[1] - s2[1])**2) ** .5)
+	plt.show()
